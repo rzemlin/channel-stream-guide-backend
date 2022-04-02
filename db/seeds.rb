fd0541839d79b9bb7bc001c8417a30f3b4e3b67f
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-02 08:23:46 -0700

    service model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+slingTv = Service.create(name: 'Sling', description: 'cheapest route', img_url: 'not yet')
+hulu = Service.create(name: 'Hulu', description: 'meh', img_url: 'not yet')
+youtubeTv = Service.create(name: 'YouTubeTv', description: 'good choice', img_url: 'not yet')
