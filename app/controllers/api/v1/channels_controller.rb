f8db61bfdd710e347c63b3d6773d61e5a05b1b54
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-03 09:55:58 -0700

    add channel controller function and routes

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::ChannelsController < ApplicationController
 
     def index
+        @channels = Channel.all
+        render json: @channels
 
     end
-    
+
+    def create
+        channel = Channel.new(channel_params)
+        if channel.save
+            render json: channel, status: :accepted
+        else
+            render json: {errors: channel.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+
+
+    private
+
+    def channel_params
+        params.require(:channel).permit(:name, :service, :genre_id, :img_url)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :channels, only: [:index]
+      resources :channels, only: [:index, :create]
     end
   end
 
