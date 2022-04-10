dea44e4f0cb088a2bc6414cb892ce4fdc33c6ef9
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-04-10 11:30:37 -0700

    add db destroy function

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Channel.destroy_all
+Genre.destroy_all
+Service.destroy_all
+
 slingTv = Service.create(name: 'Sling', description: 'cheapest route', img_url: 'not yet')
 hulu = Service.create(name: 'Hulu', description: 'meh', img_url: 'not yet')
 youtubeTv = Service.create(name: 'YouTubeTv', description: 'good choice', img_url: 'not yet')
@@ -15,4 +19,10 @@ drama = Genre.create(name:'Drama')
 comedy = Genre.create(name:'Comedy')
 action = Genre.create(name:'Action')
 
-Channel.create(name: 'ESPN', genre_id: sports.id, img_url: 'not yet', service_id: slingTv)
+Channel.create(name: 'ESPN', genre_id: sports.id, img_url: 'https://1000logos.net/espn-logo/', service_id: slingTv)
+Channel.create(name: 'CNN', genre_id: news.id, img_url: 'https://1000logos.net/espn-logo/', service_id: slingTv)
+Channel.create(name: 'Comedy Central', genre_id: comedy.id, img_url: 'https://1000logos.net/espn-logo/', service_id: hulu)
+Channel.create(name: 'Lifetime', genre_id: drama.id, img_url: 'https://1000logos.net/espn-logo/', service_id: hulu)
+Channel.create(name: 'FX', genre_id: action.id, img_url: 'https://1000logos.net/espn-logo/', service_id: youtubeTv)
+Channel.create(name: 'Fox News', genre_id: comedy.id, img_url: 'https://1000logos.net/espn-logo/', service_id: youtubeTv)
+Channel.create(name: 'CSPAN', genre_id: news.id, img_url: 'https://1000logos.net/espn-logo/', service_id: youtubeTv)
